c85063a1fec2005462970856c82b067aef1b5ad3
samithkavishke <samithkarunathilake@gmail.com>
2022-12-31 17:30:38 +0530

    UserProfile Almost done

--- Samith/client/src/App.js
+++ Samith/client/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useMemo } from "react";
-
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import PersistentDrawerLeft from "./Components/UserPageComponents/DashBoard";
 import SignIn from "./Components/LoginPageComponents/Login";
 import { LoginContext } from "./Helper/UserContext";
+import SignUp from "./Components/SignupPageComponents/Signup";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -17,6 +17,7 @@ function App() {
     <BrowserRouter>
       <LoginContext.Provider value={{ loggedIn, setLoggedIn }}>
         <Routes>
+          <Route path="/Signup" element={<SignUp />}></Route>
           <Route path="/Home" element={<PersistentDrawerLeft />}></Route>
           <Route path="/" element={<SignIn />}></Route>
         </Routes>
--- Samith/server/routes/UserProfile.jsx
+++ Samith/server/routes/UserProfile.jsx
@@ -3,23 +3,22 @@ const pool = require("../lib/pool.jsx");
 const router = express.Router();
 const cors = require("cors");
 
-router.get("/", (req, res) => {
+let employee_id = "";
+
+router.get("/:id", (req, res) => {
+  //   console.log(req.params);
   pool.query(
-    `SELECT first_name,last_name,address_line1,address_line2,town,birth_year,birth_month,birth_date,marital_status,gender FROM sql6587376.user_info WHERE username = "${req.body.username}";`,
+    `SELECT first_name,last_name,address_line1,address_line2,town,birth_year,birth_month,birth_date,marital_status,gender FROM sql6587376.employee_personal WHERE employee_id = "${req.params.id}";`,
     (err, row, field) => {
       if (err) {
         return console.log(err);
       }
       let result = JSON.parse(JSON.stringify(row));
+      console.log(result);
       if (result.length > 0) {
-        console.log(result[0].passcode_hash, req.body.password);
-
-        if (result[0].passcode_hash == req.body.password) {
-          console.log("ado hri");
-          res.send({ success: true });
-        } else {
-          res.send({ success: false });
-        }
+        // console.log(result[0].passcode_hash, req.body.password);
+        res.send({ result: result, success: true });
+        // console.log(result);
       } else {
         res.send({ success: false });
       }
